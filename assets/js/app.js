35e5050bf2363e19fea83ba92483580e2dff19f7
yamanx8 <yamanjazzar274@gmail.com>
2023-03-22 22:04:20 +0200

    initial status

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-function scrollBottom(elem) {
-    let scrollDown = elem.scrollHeight - elem.scrollTop - elem.clientHeight;
-    if (scrollDown == 0)
-        return false;
-    return scrollDown;
-}
-setInterval(() => {console.log(scrollBottom(document.getElementById("elem")));},1000);
