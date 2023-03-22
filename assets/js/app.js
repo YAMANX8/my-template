e8d83384e6a6e59b3a842b3fe195b67da5b4869a
yamanx8 <yamanjazzar274@gmail.com>
2023-03-22 22:37:57 +0200

    Revert "new sinppet"
    This reverts commit b9566c9c7a895b91a4b940fcc90dba7d726a2c1f.

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    let x = 0;
+    const obj = {
+        name: "yaman",
+        job: "software engineering"
+    };
+    return obj;
+}
+
+console.log(`${myFunction().name} I'm a ${myFunction().job} so, Hello World!!!`);
